bb851fcff48c4ce9f8a8871e26d55a04bb334887
JonCelsius <celsiusjon@gmail.com>
2022-02-01 22:08:18 +0200

    Lesson 4 Homework!

--- CoursePractice/src/HelloJavaWorld/CourseInit.java
+++ CoursePractice/src/HelloJavaWorld/CourseInit.java
@@ -10,6 +10,9 @@ public class CourseInit {
         packageOneMethod.printFromPackageOne();
         AnotherPackageMethod anotherPackageMethod=new AnotherPackageMethod();
         anotherPackageMethod.printFromSecondPackage();
+        int a=10;
+        int b= a++ + a-- + a++ + a++ + ++a - a-- + ++a + --a;
+        System.out.println(b);
       /*  float i=10.0f;
         long f= i;
         System.out.println(i);
