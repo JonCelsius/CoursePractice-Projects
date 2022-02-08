77730ce57313344e29ccc0e664b0a3b917b5b040
JonCelsius <celsiusjon@gmail.com>
2022-02-08 23:07:45 +0200

    Optimizari la FloatNumberCheck

--- CoursePractice/src/Lesson5Homework/FloatNumberCheck.java
+++ CoursePractice/src/Lesson5Homework/FloatNumberCheck.java
@@ -11,31 +11,27 @@ public class FloatNumberCheck {
         number = readValue.nextDouble();
         if (number == 0) {
             System.out.println("Numarul este zero!");
-        } else {
-            if (number > 0) {
-                if (number < 1) {
-                    System.out.println("Numarul este pozitiv si small!");
-                } else {
-                    if (number > 1000000) {
-                        System.out.println("Numarul este pozitiv si large!");
-                    } else {
-                        System.out.println("Numarul este pozitiv!");
-                    }
-                }
+        } else if (number > 0) {
+            if (number < 1) {
+                System.out.println("Numarul este pozitiv si small!");
+            } else if (number > 1000000) {
+                System.out.println("Numarul este pozitiv si large!");
             } else {
-                if (number < 0) {
-                    if (Math.abs(number) < 1) {
-                        System.out.println("Numarul este negativ si small!");
-                    } else {
-                        if (Math.abs(number) > 1000000) {
-                            System.out.println("Numarul este negativ si large!");
-                        } else {
-                            System.out.println("Numarul este negativ!");
-                        }
-                    }
-                }
+                System.out.println("Numarul este pozitiv!");
             }
+
+        } else {
+            if (Math.abs(number) < 1) {
+                System.out.println("Numarul este negativ si small!");
+            } else if (Math.abs(number) > 1000000) {
+                System.out.println("Numarul este negativ si large!");
+            } else {
+                System.out.println("Numarul este negativ!");
+            }
+
+
         }
+
         readValue.close();
     }
 
